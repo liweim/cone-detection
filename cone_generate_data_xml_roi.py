ce1fa4149796385c135b4724b566277d237a2fad
liweim <liweim@github.com>
2018-05-02 10:52:41 +0200

    annnotate roi

---
+++ cone_generate_data_xml_roi.py
@@ -0,0 +1,189 @@
+import numpy as np
+import os
+from os.path import join
+import cv2
+import argparse
+from random import random, choice
+from Utils import read_txt, write_txt, isPointsInPolygons
+from scipy.misc import imresize
+from shutil import rmtree, copyfile
+import glob
+import xml.etree.ElementTree as ET
+import pandas as pd
+
+patch_size = 25
+resize_rate = 0.5
+factor0 = 8
+factor100 = 12
+radius = int((patch_size-1)/2)
+
+# patch_size = 45
+# resize_rate = 1
+# factor0 = 20
+# factor100 = 24
+# radius = int((patch_size-1)/2)
+
+def random_shift(x, y, max_pixel):
+    x_shift = x + choice(range(-max_pixel, max_pixel))
+    y_shift = y + choice(range(-max_pixel, max_pixel))
+    return x_shift, y_shift
+
+def augmentation(img):
+    #zoom
+    random_rate = random()
+    if random_rate > 0.7:
+        R = int(patch_size*random_rate/2)
+        img = img[radius-R:radius+R, radius-R:radius+R]
+        img = cv2.resize(img, (patch_size, patch_size))
+    return img
+
+def generate_data_xml(annotation_paths, data_path):
+    if os.path.exists(data_path):
+        rmtree(data_path)
+    # os.mkdir(data_path)
+    for i in range(5):
+        os.makedirs(join(data_path, 'train', str(i)))
+        os.makedirs(join(data_path, 'test', str(i)))
+    classes = ['background', 'blue', 'yellow', 'orange', 'orange2']
+
+    count0 = 0
+    column_name = ['x', 'y', 'ratio', 'label']
+    train_imgs = []
+    for annotation_path in annotation_paths:
+        xml_path = join(annotation_path, '*.xml')
+        for xml_file in glob.glob(xml_path):
+            print(xml_file)
+            tree = ET.parse(xml_file)
+            root = tree.getroot()
+            basename = os.path.splitext(xml_file)[0]
+            img_path = basename+'.png'
+
+            img = cv2.imread(img_path)
+            img = imresize(img, 1.0*resize_rate)
+
+            row, col = img.shape[:2]
+            mask = np.zeros(img.shape[:2]).astype(np.uint8)
+            cones = []
+            
+            for member in root.findall('object'):
+                label = member[0].text
+                x1 = int(int(member[4][0].text) * resize_rate)
+                y1 = int(int(member[4][1].text) * resize_rate)
+                x2 = int(int(member[4][2].text) * resize_rate)
+                y2 = int(int(member[4][3].text) * resize_rate)
+                x = int((x1+x2)/2)
+                y = int((y1+y2)/2)
+
+                # mask[y1:y2,x1:x2] = 0
+                mask[max(0,y1-10):min(row,y2+10),max(0,x1-10):min(col,x2+10)] = 100
+                mask[max(0,y1-3):min(row,y2+3),max(0,x1-3):min(col,x2+3)] = 0
+                
+                max_length = max(abs(x2-x1), abs(y2-y1)) * 1.5
+                ratio = max_length/patch_size
+                if ratio > 0.4:
+                    cones.append([x, y, label, ratio])
+
+            txt_path = basename+'.csv'
+            column_name = ['x', 'y', 'label', 'ratio']
+            cone_df = pd.DataFrame(cones, columns=column_name)
+            cone_df.to_csv(txt_path, index=None, header=False)
+
+            for x, y, label, ratio in cones:
+                if label == 'blue':
+                    cv2.circle(mask, (x, y), 2, 252, -1)
+                if label == 'yellow':
+                    cv2.circle(mask, (x, y), 2, 253, -1)
+                if label == 'orange':
+                    cv2.circle(mask, (x, y), 2, 254, -1)
+                if label == 'orange2':
+                    cv2.circle(mask, (x, y), 2, 255, -1)
+            mask[:radius, :] = 0
+            mask[img.shape[0]-radius:, :] = 0
+            mask[:, :radius] = 0
+            mask[:, img.shape[1]-radius:] = 0
+
+            # cv2.namedWindow('mask', cv2.WINDOW_NORMAL)
+            # cv2.imshow('mask', mask)
+            # cv2.namedWindow('img', cv2.WINDOW_NORMAL)
+            # cv2.imshow('img', img)
+            # cv2.waitKey(0)
+
+            extra_back = 0
+            while extra_back<20:
+                r = choice(range(row))
+                c = choice(range(col))
+                if mask[r,c] == 100:
+                    image = img[r-radius:r+radius+1, c-radius:c+radius+1]
+                    image = cv2.resize(image, (patch_size, patch_size))
+                    if random() < 0.7:
+                        path = join(data_path, 'train/0')
+                        # train_imgs.append(image)
+                    else:
+                        path = join(data_path, 'test/0')
+                    num = len(os.listdir(path))
+                    cv2.imwrite(join(path, str(num)+'.png'), image)
+                    extra_back += 1
+
+            for r in range(row):
+                for c in range(col):               
+                    if mask[r,c] > 100:
+                        image = img[r-radius:r+radius+1, c-radius:c+radius+1]
+                        image = cv2.resize(image, (patch_size, patch_size))
+                        if random() < 0.7:
+                            image = augmentation(image)
+                            path = join(data_path, 'train')
+                        else:
+                            path = join(data_path, 'test')
+                        if mask[r,c] == 252:
+                            path = join(path, '1')
+                        if mask[r,c] == 253:
+                            path = join(path, '2')
+                        if mask[r,c] == 254:
+                            path = join(path, '3')
+                        if mask[r,c] == 255:
+                            path = join(path, '4')
+                        num = len(os.listdir(path))
+                        cv2.imwrite(join(path, str(num)+'.png'), image)
+
+    background_folder = 'annotations/background'
+    path = join(background_folder, '*.txt')
+    for txt_path in glob.glob(path):
+        img_path = txt_path[:-3]+'png'
+        img = cv2.imread(img_path)
+        points = read_txt(txt_path)
+        row, col = img.shape[:2]
+        for point in points[1:]:
+            x = int(point[0])
+            y = int(point[1])
+            image = img[max(y-radius,0):min(y+radius+1,row), max(x-radius,0):min(x+radius+1,col), :]
+            image = cv2.resize(image, (patch_size, patch_size))
+            if random() < 0.7:
+                image = augmentation(image)
+                path = join(data_path, 'train', '0')
+                train_imgs.append(image)
+            else:
+                path = join(data_path, 'test', '0')
+            num = len(os.listdir(path))
+            if np.max(image)>0:
+                cv2.imwrite(join(path, str(num)+'.png'), image)
+                # print(img_path)
+                # cv2.namedWindow('mask', cv2.WINDOW_NORMAL)
+                # cv2.imshow('mask', image)
+                # cv2.waitKey(0)
+
+    for i in range(5):
+        path = join(data_path, 'train', str(i))
+        print('{}: {}'.format(classes[i], len(os.listdir(path))))
+
+    # train_imgs = np.array(train_imgs)
+    # print(np.mean(train_imgs[:,:,:,0]))
+    # print(np.mean(train_imgs[:,:,:,1]))
+    # print(np.mean(train_imgs[:,:,:,2]))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--annotation_paths', nargs='+')
+    parser.add_argument('--data_path', type=str)
+    args = parser.parse_args()
+
+    generate_data_xml(annotation_paths = args.annotation_paths, data_path = args.data_path)
--- sift_cnn.cpp
+++ sift_cnn.cpp
@@ -7,6 +7,91 @@
 
 tiny_dnn::network<tiny_dnn::sequential> m_slidingWindow;
 
+void blockMatching(cv::Mat &disp, cv::Mat imgL, cv::Mat imgR){
+  cv::Mat grayL, grayR;
+
+  cv::cvtColor(imgL, grayL, 6);
+  cv::cvtColor(imgR, grayR, 6);
+
+  cv::Ptr<cv::StereoBM> sbm = cv::StereoBM::create(); 
+  sbm->setBlockSize(17);
+  sbm->setNumDisparities(32);
+
+  sbm->compute(grayL, grayR, disp);
+  cv::normalize(disp, disp, 0, 255, 32, CV_8U);
+}
+
+void reconstruction(cv::Mat img, cv::Mat &Q, cv::Mat &disp, cv::Mat &rectified, cv::Mat &XYZ){
+  // cv::Mat mtxLeft = (cv::Mat_<double>(3, 3) <<
+  //   350.6847, 0, 332.4661,
+  //   0, 350.0606, 163.7461,
+  //   0, 0, 1);
+  // cv::Mat distLeft = (cv::Mat_<double>(5, 1) << -0.1674, 0.0158, 0.0057, 0, 0);
+  // cv::Mat mtxRight = (cv::Mat_<double>(3, 3) <<
+  //   351.9498, 0, 329.4456,
+  //   0, 351.0426, 179.0179,
+  //   0, 0, 1);
+  // cv::Mat distRight = (cv::Mat_<double>(5, 1) << -0.1700, 0.0185, 0.0048, 0, 0);
+  // cv::Mat R = (cv::Mat_<double>(3, 3) <<
+  //   0.9997, 0.0015, 0.0215,
+  //   -0.0015, 1, -0.00008,
+  //   -0.0215, 0.00004, 0.9997);
+  // cv::Mat T = (cv::Mat_<double>(3, 1) << -119.1807, 0.1532, 1.1225);
+  // cv::Size stdSize = cv::Size(640, 360);
+
+  //official
+  cv::Mat mtxLeft = (cv::Mat_<double>(3, 3) <<
+    349.891, 0, 334.352,
+    0, 349.891, 187.937,
+    0, 0, 1);
+  cv::Mat distLeft = (cv::Mat_<double>(5, 1) << -0.173042, 0.0258831, 0, 0, 0);
+  cv::Mat mtxRight = (cv::Mat_<double>(3, 3) <<
+    350.112, 0, 345.88,
+    0, 350.112, 189.891,
+    0, 0, 1);
+  cv::Mat distRight = (cv::Mat_<double>(5, 1) << -0.174209, 0.026726, 0, 0, 0);
+  cv::Mat rodrigues = (cv::Mat_<double>(3, 1) << -0.0132397, 0.021005, -0.00121284);
+  cv::Mat R;
+  cv::Rodrigues(rodrigues, R);
+  cv::Mat T = (cv::Mat_<double>(3, 1) << -0.12, 0, 0);
+  cv::Size stdSize = cv::Size(672, 376);
+
+  int width = img.cols;
+  int height = img.rows;
+  cv::Mat imgL(img, cv::Rect(0, 0, width/2, height));
+  cv::Mat imgR(img, cv::Rect(width/2, 0, width/2, height));
+
+  cv::resize(imgL, imgL, stdSize);
+  cv::resize(imgR, imgR, stdSize);
+
+  //std::cout << imgR.size() <<std::endl;
+
+  cv::Mat R1, R2, P1, P2;
+  cv::Rect validRoI[2];
+  cv::stereoRectify(mtxLeft, distLeft, mtxRight, distRight, stdSize, R, T, R1, R2, P1, P2, Q,
+    cv::CALIB_ZERO_DISPARITY, 0.0, stdSize, &validRoI[0], &validRoI[1]);
+
+  cv::Mat rmap[2][2];
+  cv::initUndistortRectifyMap(mtxLeft, distLeft, R1, P1, stdSize, CV_16SC2, rmap[0][0], rmap[0][1]);
+  cv::initUndistortRectifyMap(mtxRight, distRight, R2, P2, stdSize, CV_16SC2, rmap[1][0], rmap[1][1]);
+  cv::remap(imgL, imgL, rmap[0][0], rmap[0][1], cv::INTER_LINEAR);
+  cv::remap(imgR, imgR, rmap[1][0], rmap[1][1], cv::INTER_LINEAR);
+
+  // cv::imwrite("tmp/imgL.png", imgL);
+  // cv::imwrite("tmp/imgR.png", imgR);
+  // return;
+
+  blockMatching(disp, imgL, imgR);
+
+  // cv::namedWindow("disp", cv::WINDOW_NORMAL);
+  // cv::imshow("disp", disp);
+  // cv::waitKey(0);
+
+  rectified = imgL;
+
+  cv::reprojectImageTo3D(disp, XYZ, Q);
+}
+
 void convertImage(cv::Mat img, int w, int h, tiny_dnn::vec_t &data){
   cv::Mat resized;
   cv::resize(img, resized, cv::Size(w, h));
@@ -21,7 +106,7 @@ void convertImage(cv::Mat img, int w, int h, tiny_dnn::vec_t &data){
   }
 }
 
-void slidingWindow(const std::string &dictionary) {
+void slidingWindow(const std::string& dictionary) {
   using conv    = tiny_dnn::convolutional_layer;
   using pool    = tiny_dnn::max_pooling_layer;
   using fc      = tiny_dnn::fully_connected_layer;
@@ -45,7 +130,7 @@ void slidingWindow(const std::string &dictionary) {
   ifs >> m_slidingWindow;
 }
 
-void siftCNN(cv::Mat rectified, std::vector<cv::KeyPoint> pts, std::vector<int>& outputs){
+void forwardDetectionRoI(const std::string& img_path){
   //Given RoI by SIFT detector and detected by CNN
   float_t threshold = 0.7;
   int radius = 12;
@@ -53,17 +138,41 @@ void siftCNN(cv::Mat rectified, std::vector<cv::KeyPoint> pts, std::vector<int>&
   std::vector<tiny_dnn::tensor_t> inputs;
   std::vector<int> verifiedIndex;
   std::vector<cv::Vec3i> porperty;
-  outputs.clear();
+  // std::vector<int> outputs;
 
-  for(size_t i = 0; i < pts.size(); i++){
-    int x = pts[i].pt.x;
-    int y = pts[i].pt.y;
+  cv::Mat imgSource = cv::imread(img_path);
+	cv::Mat Q, disp, XYZ, img;
+	reconstruction(imgSource, Q, disp, img, XYZ);
+	// img = img.rowRange(176, 376);
+	img = img.rowRange(140, 270);
+	img.rowRange(0,24) = 0;
+	img.rowRange(106,130) = 0;
+	cv::Mat img_hsv;
+	cv::cvtColor(img, img_hsv, cv::COLOR_BGR2HSV);
+
+	cv::Ptr<cv::Feature2D> detector = cv::xfeatures2d::SIFT::create(15);
+	std::vector<cv::KeyPoint> keypoints;
+	detector->detect(img_hsv, keypoints);
+
+	cv::Mat Match;
+	cv::drawKeypoints(img, keypoints, Match);
+
+	// cv::namedWindow("cv::Match", cv::WINDOW_NORMAL);
+	// cv::imshow("cv::Match", Match);
+	// cv::waitKey(0);
+
+	cv::resize(img, img, cv::Size(336, 65));
+	// cv::resize(img, img, cv::Size(336, 100));
+
+  for(size_t i = 0; i < keypoints.size(); i++){
+    int x = keypoints[i].pt.x/2;
+    int y = keypoints[i].pt.y/2;
 
 	cv::Rect roi;
 	roi.x = std::max(x - radius, 0);
 	roi.y = std::max(y - radius, 0);
-	roi.width = std::min(x + radius, rectified.cols) - roi.x;
-	roi.height = std::min(y + radius, rectified.rows) - roi.y;
+	roi.width = std::min(x + radius, img.cols) - roi.x;
+	roi.height = std::min(y + radius, img.rows) - roi.y;
 
 	//cv::circle(img, cv::Point (x,y), radius, cv::Scalar (0,0,0));
 	// // cv::circle(disp, cv::Point (x,y), 3, 0, CV_FILLED);
@@ -71,16 +180,16 @@ void siftCNN(cv::Mat rectified, std::vector<cv::KeyPoint> pts, std::vector<int>&
 	//cv::imshow("roi", img);
 	//cv::waitKey(0);
 	//cv::destroyAllWindows();
-	if (0 > roi.x || 0 > roi.width || roi.x + roi.width > rectified.cols || 0 > roi.y || 0 > roi.height || roi.y + roi.height > rectified.rows){
+	if (0 > roi.x || 0 > roi.width || roi.x + roi.width > img.cols || 0 > roi.y || 0 > roi.height || roi.y + roi.height > img.rows){
 		std::cout << "Wrong roi!" << std::endl;
-		outputs.push_back(-1);
+		// outputs.push_back(-1);
 	}
 	else{
-		auto patchImg = rectified(roi);
+		auto patchImg = img(roi);
 		tiny_dnn::vec_t data;
 		convertImage(patchImg, 25, 25, data);
 		inputs.push_back({data});
-		outputs.push_back(0);
+		// outputs.push_back(0);
 		verifiedIndex.push_back(i);
 		porperty.push_back(cv::Vec3i(x,y,radius));
 	}
@@ -97,67 +206,52 @@ void siftCNN(cv::Mat rectified, std::vector<cv::KeyPoint> pts, std::vector<int>&
           maxProb = prob[i][0][j];
         }
       }
-      outputs[verifiedIndex[i]] = maxIndex;
+      // outputs[verifiedIndex[i]] = maxIndex;
       int x = int(porperty[i][0]);
       int y = int(porperty[i][1]);
-      float_t radius = 2;
+      float_t radius = 1;
 
       std::string labels[] = {"blue", "yellow", "orange", "big orange"};
       if (maxIndex == 0 || maxProb < threshold){
         std::cout << "No cone detected" << std::endl;
-        cv::circle(rectified, cv::Point (x,y), radius, cv::Scalar (0,0,0), -1);
+        cv::circle(img, cv::Point (x,y), radius, cv::Scalar (0,0,0), -1);
       } 
       else{
         std::cout << "Find one " << labels[maxIndex-1] << " cone"<< std::endl;
         if (labels[maxIndex-1] == "blue")
-          cv::circle(rectified, cv::Point (x,y), radius, cv::Scalar (255,0,0), -1);
+          cv::circle(img, cv::Point (x,y), radius, cv::Scalar (255,0,0), -1);
         else if (labels[maxIndex-1] == "yellow")
-          cv::circle(rectified, cv::Point (x,y), radius, cv::Scalar (0,255,255), -1);
+          cv::circle(img, cv::Point (x,y), radius, cv::Scalar (0,255,255), -1);
         else if (labels[maxIndex-1] == "orange")
-          cv::circle(rectified, cv::Point (x,y), radius, cv::Scalar (0,165,255), -1);
+          cv::circle(img, cv::Point (x,y), radius, cv::Scalar (0,165,255), -1);
         else if (labels[maxIndex-1] == "big orange")
-          cv::circle(rectified, cv::Point (x,y), radius*2, cv::Scalar (0,0,255), -1);
+          cv::circle(img, cv::Point (x,y), radius*2, cv::Scalar (0,0,255), -1);
       }
     }
   }
 
-  // cv::namedWindow("disp", cv::WINDOW_NORMAL);
-  // cv::imshow("disp", rectified);
-  // cv::waitKey(0);
+  cv::namedWindow("disp", cv::WINDOW_NORMAL);
+  cv::imshow("disp", img);
+  cv::waitKey(0);
   // cv::destroyAllWindows();
 
   // for(size_t i = 0; i < pts.size(); i++)
   //   std::cout << i << ": " << outputs[i] << std::endl;
 }
 
+void extractFeature(){
+
+}
+
 
 int main( int argc, char** argv )
 {
 
-	slidingWindow("models/all_rgb_best");
-	for(int i = 0; i < 316; i++){
+	slidingWindow("models/all_rgb");
+	for(int i = 1; i < 316; i++){
 		auto startTime = std::chrono::system_clock::now();
-
-		cv::Mat img = cv::imread("annotations/skidpad1/rectified/"+std::to_string(i)+".png");
-		img = img.rowRange(150, 270);
-		img.rowRange(0,12) = 0;
-		img.rowRange(108,120) = 0;
-		cv::Mat img_hsv;
-		cv::cvtColor(img, img_hsv, cv::COLOR_BGR2HSV);
-
-		cv::Ptr<cv::Feature2D> detector = cv::xfeatures2d::SIFT::create(20);
-		std::vector<cv::KeyPoint> keypoints;
-		detector->detect(img_hsv, keypoints);
+		forwardDetectionRoI("annotations/rainy/images/"+std::to_string(i)+".png");
 		
-		cv::Mat Match;
-		cv::drawKeypoints(img, keypoints, Match);
-
-		// cv::namedWindow("cv::Match", cv::WINDOW_NORMAL);
-		// cv::imshow("cv::Match", Match);
-		// cv::waitKey(0);
-
-		std::vector<int> outputs;
-		siftCNN(img, keypoints, outputs);
 
 		auto endTime = std::chrono::system_clock::now();
 	  	std::chrono::duration<double> diff = endTime-startTime;
